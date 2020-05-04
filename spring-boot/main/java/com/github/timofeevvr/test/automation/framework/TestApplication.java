010796e63e132c39ef8e1897e5660655c49c4b79
Valerii_Timofeev <Valerii_Timofeev@epam.com>
2020-05-04 17:34:08 +0300

    init

---
+++ spring-boot/main/java/com/github/timofeevvr/test/automation/framework/TestApplication.java
@@ -0,0 +1,13 @@
+package com.github.timofeevvr.test.automation.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
