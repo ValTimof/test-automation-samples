49c76a8eb44e8b309eaf5178aa6896cb4759324e
Valerii_Timofeev <Valerii_Timofeev@epam.com>
2020-06-10 18:40:20 +0300

    wip

--- spring-boot/src/main/java/com/github/timofeevvr/test/automation/framework/TestApplication.java
+++ spring-boot/src/main/java/com/github/timofeevvr/test/automation/framework/TestApplication.java
@@ -1,13 +1,7 @@
 package com.github.timofeevvr.test.automation.framework;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
-
 }
--- spring-boot/src/test/java/com/github/timofeevvr/test/automation/framework/ApplicationContextTests.java
+++ spring-boot/src/test/java/com/github/timofeevvr/test/automation/framework/ApplicationContextTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class ApplicationContextTests {
 
@@ -14,7 +16,7 @@ class ApplicationContextTests {
 	@Test
 	void givenSpringBoot_whenAddAnnotations_thenContextIsLoaded() {
 		//when
-
+		assertThat(true).isTrue();
 		//then
 	}
 
